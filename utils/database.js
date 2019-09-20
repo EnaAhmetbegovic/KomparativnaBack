9c5a02185dd62b2b94a0aea8e720b8044b753607
EnaAhmetbegovic <ena.ahmetbegovic@gmail.com>
2019-09-20 10:25:11 +0200

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,13 @@
 const model = require('../models/model');
 
-const addOrder = async function (name, surname, mass, volume, address, latitude, longitude) {
+const addOrder = async function (
+    name, surname, orderMass, orderVolume, address, latitude, longitude
+) {
     return await model.Order.create({
         name: name,
         surname: surname,
-        mass: mass,
-        volume: volume,
+        orderMass: orderMass,
+        orderVolume: orderVolume,
         address: address,
         latitude: latitude,
         longitude: longitude
@@ -16,25 +18,93 @@ const addOrder = async function (name, surname, mass, volume, address, latitude,
     });
 };
 
+
 const getAllOrders = async function () {
     return await model.Order
         .findAll()
         .then( rows => {
             var orders = [];
             rows.forEach(function (row) {
-                orders.push(row.dataValues.id);
+                orders.push({
+                    id : row.dataValues.id,
+                    name : row.dataValues.name,
+                    surname : row.dataValues.surname,
+                    orderMass : row.dataValues.orderMass,
+                    orderVolume : row.dataValues.orderVolume,
+                    address : row.dataValues.address,
+                    latitude : row.dataValues.latitude,
+                    longitude : row.dataValues.longitude
+                });
             });
             return orders;
         });
 };
 
-const deleteOrder = async function (orderId) {
-    return await model.sequelize.query(
-        'DELETE FROM Order WHERE id = orderId'
-    ).then( result => {
-        console.log(result);
-        return 0;
-    })
+const getDeliveryRoute = async function () {
+    return await model.Order
+        .findAll()
+        .then(rows => {
+            var coordinates = [];
+            var ordersCoordinates = [];
+            var length = model.Order.length;
+            var random = Math.random() * (100) + length;
+            var startingPoint = model.Order[random];
+
+            rows.forEach(function (row) {
+                coordinates.push({
+                    id : row.dataValues.id,
+                    latitude : row.dataValues.latitude,
+                    longitude : row.dataValues.longitude
+                });
+            });
+
+            var pomLat = 0;
+            var pomLon = 0;
+
+            // Ako je ravna ploča :P
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < length-1; j++) {
+                    if (
+                        Math.sqrt(
+                            coordinates.longitude[i] - coordinates.longitude[j] * coordinates.longitude[i] - coordinates.longitude[j] +
+                            coordinates.latitude[i] - coordinates.latitude[j] * coordinates.latitude[i] - coordinates.latitude[j]
+                        ) >
+                        Math.sqrt(
+                            coordinates.longitude[i] - coordinates.longitude[j+1] * coordinates.longitude[i] - coordinates.longitude[j+1] +
+                            coordinates.latitude[i] - coordinates.latitude[j+1] * coordinates.latitude[i] - coordinates.latitude[j+1]
+                        )
+                    ) {
+                        pomLat = coordinates.latitude[j+1];
+                        pomLon = coordinates.longitude[j+1];
+                        coordinates.latitude[j+1] = coordinates.latitude[i];
+                        coordinates.longitude[j+1] = coordinates.longitude[i];
+                        coordinates.latitude[i] = coordinates.latitude[pomLat];
+                        coordinates.longitude[i] = coordinates.longitude[pomLon];
+                    }
+                }
+            }
+
+            for (let i = startingPoint-1; i < length+startingPoint; i++) {
+                ordersCoordinates.push({
+                    id : (i%coordinates.length).dataValues.id,
+                    latitude : (i%coordinates.length).dataValues.latitude,
+                    longitude : (i%coordinates.length).dataValues.longitude,
+                    route : (i%coordinates.length)+1
+                });
+            }
+
+            return ordersCoordinates;
+        });
+};
+
+const deleteOrder = async function (id) {
+    model.Order.destroy({
+        where: {
+            id: id,
+        }
+    }).then(res => {
+        console.log("Order is deleted");
+    });
 };
 
 module.exports = {
